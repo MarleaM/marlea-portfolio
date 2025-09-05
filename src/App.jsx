a617585894ce18e46f57693383c565ab40e2071e
Marlea <marleamartens1@gmail.com>
2025-09-05 14:25:21 -0400

    kill me

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
 
   return (
-    <Router basename={"/"}>
+    <Router>
       <Routes>
         <Route path="/" element={
           <div>
