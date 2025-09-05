f7095bc7ec0760fa1d7ce4c2355adb6d29318e6d
Marlea <marleamartens1@gmail.com>
2025-09-05 13:56:33 -0400

    still trying to fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
 
   return (
-    <Router basename={import.meta.env.BASE_URL}>
+    <Router basename={"/"}>
       <Routes>
         <Route path="/" element={
           <div>
